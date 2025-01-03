525428531b39839b2d9e12892c648d0bf17a8a88
TIIKKETMASTER <112451827+TIIKKETMASTER@users.noreply.github.com>
2025-01-03 02:04:32 -0800

    uploading work

---
+++ questions.js
@@ -0,0 +1,5 @@
+questions =  [
+    {"id": 0, "question": "prog", "effects": {"prog": 1}},
+    {"id": 1, "question": "auth", "effects": {"auth": 1}},
+    {"id": 2, "question": "right", "effects": {"right": 1}}
+];
