a8d25d4670934057865e1f02c1604fab41cb1dff
TIIKKETMASTER <112451827+TIIKKETMASTER@users.noreply.github.com>
2025-08-30 06:19:13 +0000

    major graphical update -- Beta 0.8.0 seriously dont fucking touch this on phones man this shit sucks ass on phone just stop

--- qAL.js
+++ qAL.js
@@ -1,14 +1,14 @@
 questions =  [
-    {"id": 0, "question": "Courts and law firms should be privatized.", "effects": {"auth": -1, "prog":0,"right":0}},
-    {"id": 1, "question": "Governmental surveillance is essential for a secure state.", "effects": {"auth": 1}},
-    {"id": 2, "question": "If the government were to exist, it should ideally be minimized to only bare functions.", "effects": {"auth": -1}},
-    {"id": 3, "question": "The government is less efficient than private organizations in managing resources and providing services.", "effects": {"auth": -1}},
-    {"id": 4, "question": "Without the state, life would be solitary, poor, nasty, brutish, and short.", "effects": {"auth": 1}},
-    {"id": 5, "question": "People should be able to do whatever they want with their own bodies.", "effects": {"auth": -1}},
-    {"id": 6, "question": "Local communities and governments are better than large, centralized states.", "effects": {"auth": -1}},
-    {"id": 7, "question": "Identification techniques should be used to ensure public safety.", "effects": {"auth": 1}},
-    {"id": 8, "question": "Strong, centralized governments are ideal.", "effects": {"auth": 1}},
-    {"id": 9, "question": "The government should have the authority to regulate or influence the interactions of two consenting parties.", "effects": {"auth": 1.5}},
-    {"id": 10, "question": "The government should be used to incentivize people towards or away from certain actions.", "effects": {"auth": 1}},
-    {"id": 11, "question": "It is the government's responsibility to enforce morality.", "effects": {"auth": 1.5}}
+    {"id": 14, "question": "Identification and surveillance techniques should be used to ensure public safety.", "effects": {"auth": 1}},
+    {"id": 15, "question": "The government should have the authority to regulate or influence any interactions between two or more consenting parties.", "effects": {"auth": 1}},
+    {"id": 16, "question": "The government should be used to incentivize people towards or away from certain actions.", "effects": {"auth": 1}},
+    {"id": 17, "question": "It is the government's responsibility to enforce morality.", "effects": {"auth": 1}},
+    {"id": 18, "question": "Individualism is an unhealthy scourge of a culture.", "effects": {"auth": 1}},
+    {"id": 19, "question": "The government is the only entity that can fairly and effectively regulate institutions and organizations.", "effects": {"auth": 1}},
+    {"id": 20, "question": "Smaller, local communities or governments are preferable to large, centralized states.", "effects": {"auth": -1}},
+    {"id": 21, "question": "People should be able to do whatever they want with their own bodies.", "effects": {"auth": -1}},
+    {"id": 22, "question": "If the government must exist, it ought to be minimized to only bare functions such as emergency services, law & defense, or welfare.", "effects": {"auth": -1}},
+    {"id": 23, "question": "The state ought to be a means to an end; it (likely) should not last in its current size forever.", "effects": {"auth": -1}},
+    {"id": 24, "question": "Acts and organizations, like jury duty or public schools, made compulsory by the state, are evil and should be abolished.", "effects": {"auth": -1}},
+    {"id": 25, "question": "Large state bureaucracies & administrations are wasteful/evil and should be minimized or abolished whenever possible.", "effects": {"auth": -1}}
 ];
